a3d2a23115fe120522cf9688876f80c51a2d99b6
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-21 11:34:31 -0300

    introdução do styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import "./App.css";
+import styled from "styled-components";
 import Header from "./components/Header";
 
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589);
+`;
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <AppContainer>
+      <Header />
+    </AppContainer>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,19 @@
+import styled from "styled-components";
 import MenuHeaderIcon from "../MenuHeaderIcon";
 import MenuHeaderItem from "../MenuHeaderItem";
 import Logo from "../logo";
-import "./estilo.css";
-
+const HeaderContainer = styled.header`
+  background-color: white;
+  display: flex;
+  justify-content: center;
+`;
 function Header() {
   return (
-    <header className="App-header">
+    <HeaderContainer className="App-header">
       <Logo />
       <MenuHeaderItem />
       <MenuHeaderIcon />
-    </header>
+    </HeaderContainer>
   );
 }
 
